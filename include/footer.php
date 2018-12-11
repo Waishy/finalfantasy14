f668642eed3589ac866ffce703f2d930f0ba01c2
Vincent <the-dede@hotmail.fr>
2018-12-11 11:56:08 +0100

    Menu
    Le menu est fini

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+
+
+</body>
+
+  <script src="js/mlpushmenu.js"></script>
+<script src="js/classie.js"></script>
+<script>
+	new mlPushMenu(document.getElementById('mp-menu'), document.getElementById('trigger'));
+</script>
+</html>
---
+++ include/header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Test menu de la guilde</title>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+	 crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="css/demo.css" />
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
+    <script src="js/modernizr.custom.js"></script>
+  
+</head>
+
+<body>
+	<div class="container">
+		<!-- Push Wrapper -->
+		<div class="mp-pusher" id="mp-pusher">
+
+			<!-- mp-menu -->
+			<nav id="mp-menu" class="mp-menu">
+				<div class="mp-level">
+					<h2>Menu Principal</h2>
+					<ul>
+						<li>
+							<a href="#"><i class="fas fa-home"></i> <span class="nav-menu">Accueil</span></a>
+						</li>
+						<li>
+							<a href="#"><i class="fas fa-user-ninja"></i></i> <span class="nav-menu">Membres</span></a>
+						</li>
+						<li>
+							<a href="#"><i class="fas fa-book-dead"></i><span class="nav-menu">News
+								</span></a>
+							<div class="mp-level">
+								<h2>News</h2>
+								<ul>
+									<li>
+										<a href="#"><i class="fas fa-archive"></i><span class="nav-menu">Archives
+											</span></a>
+									</li>
+									<li>
+										<a href="#"><i class="far fa-newspaper"></i><span class="nav-menu">Actualités du Jeu
+											</span></a>
+									</li>
+									<li>
+
+										<a href="#"><i class="fas fa-bullhorn"></i><span class="nav-menu">Annonce
+											</span></a>
+
+									</li>
+									<li>
+										<a href="#"><i class="fas fa-calendar-alt"></i><span class="nav-menu">Planning
+											</span></a>
+									</li>
+								</ul>
+							</div>
+						</li>
+						<li><a href="#"><i class="far fa-images"></i><span class="nav-menu">Gallerie
+								</span></a></li>
+						<li><a href="#"><i class="fas fa-award"></i><span class="nav-menu">Recrutement
+								</span></a></li>
+					</ul>
+				</div>
+			</nav>
+			<div class="nav-push">
+				<i class="fas fa-bars" id="trigger" style="padding: 15px; font-size:2em;"></i>
+			</div>
+		</div><!-- /pusher -->
+	</div><!-- /container -->
+
