2f7bc804518af18ad2265a213550023daf576682
Vincent <the-dede@hotmail.fr>
2018-12-18 08:56:56 +0100

    test ça

--- include/footer.php
+++ include/footer.php
@@ -1,10 +1,5 @@
 
 
 </body>
-
-  <script src="js/mlpushmenu.js"></script>
-<script src="js/classie.js"></script>
-<script>
-	new mlPushMenu(document.getElementById('mp-menu'), document.getElementById('trigger'));
-</script>
+<script src="./js/menu.js"></script>
 </html>
--- include/header.php
+++ include/header.php
@@ -11,7 +11,6 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
 	 crossorigin="anonymous">
 	<link rel="stylesheet" href="./css/index.css">
-	<script src="./js/menu.js"></script>
 
 </head>
 
@@ -25,30 +24,32 @@
 		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
 
 		<a href="#">
-			<div class="nav-menu">Accueil</div><i class="fas fa-home"></i>
+			<span>Accueil</span><i class="fas fa-home"></i>
 		</a>
 		<a href="#">
-			<div class="nav-menu">Membres</div><i class="fas fa-user-ninja"></i>
-		</a>
-
-		<a href="#">
-			<div class="nav-menu">News
-			</div><i class="fas fa-book-dead"></i>
+			<span>Membres</span><i class="fas fa-user-ninja"></i>
 		</a>
 
 		<a href="#">
-			<div class="nav-menu">Planning
-			</div><i class="fas fa-calendar-alt"></i>
+			<span>News</span>
+			<i class="fas fa-book-dead"></i>
 		</a>
 
 		<a href="#">
-			<div class="nav-menu">Gallerie
-			</div><i class="far fa-images"></i>
+			<span>Planning</span>
+			<i class="fas fa-calendar-alt"></i>
+		</a>
+
+		<a href="#">
+			<span>Gallerie</span>
+			<i class="far fa-images"></i>
 		</a><a href="#">
-			<div class="nav-menu">Recrutement
-			</div><i class="fas fa-award"></i>
+			<span>Recrutement</span>
+			<i class="fas fa-award"></i>
 		</a>
 	</div>
 	<i class="fas fa-bars" id="trigger" style="padding: 15px; font-size:2em;" onclick="openNav()"></i>
 
-	
+	<div class="main">
+
+	</div>
